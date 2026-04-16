f11fcdd947e06153c746f23d5067c9b3a38df3b7
Erwin Gano <erwingano11@gmail.com>
2026-04-16 23:31:15 +0800

    ENV Update

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,9 +1,13 @@
+require("dotenv").config({
+  path: "/home/Erwin/XPS-CRM-ANALYTICS/backend/.env",
+});
+
 module.exports = {
   apps: [
     {
       name: "crm-analytics-backend",
-      script: "./backend/src/server.js",
-      cwd: "/opt/CRMAnalytrics",
+      script: "src/server.js",
+      cwd: "/home/Erwin/XPS-CRM-ANALYTICS/backend",
       instances: "max",
       exec_mode: "cluster",
       watch: false,
@@ -11,6 +15,20 @@ module.exports = {
       env: {
         NODE_ENV: "production",
         PORT: 3001,
+        SUITECRM_URL: process.env.SUITECRM_URL,
+        SUITECRM_USERNAME: process.env.SUITECRM_USERNAME,
+        SUITECRM_PASSWORD: process.env.SUITECRM_PASSWORD,
+        CLIENT_ID: process.env.CLIENT_ID,
+        CLIENT_SECRET: process.env.CLIENT_SECRET,
+        OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+        MYSQL_HOST: process.env.MYSQL_HOST,
+        MYSQL_PORT: process.env.MYSQL_PORT,
+        MYSQL_USER: process.env.MYSQL_USER,
+        MYSQL_PASSWORD: process.env.MYSQL_PASSWORD,
+        MYSQL_DATABASE: process.env.MYSQL_DATABASE,
+        GOOGLE_CLIENT_ID: process.env.GOOGLE_CLIENT_ID,
+        GOOGLE_CLIENT_SECRET: process.env.GOOGLE_CLIENT_SECRET,
+        JWT_SECRET: process.env.JWT_SECRET,
       },
       error_file: "/var/log/pm2/crm-analytics-error.log",
       out_file: "/var/log/pm2/crm-analytics-out.log",
